92a82c74b55093f9c30a30363f1e93af1fc20d89
Rafael Pena <concorsfin@gmail.com>
2019-05-26 23:10:12 +0000

    Added profile edit action and view

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -18,6 +18,13 @@ class ProfilesController < ApplicationController
       render action: :new
     end
   end
+  
+  # GET request to /users/:user_id/profiles/edit
+  def edit
+    @user = User.find( params[:user_id] )
+    @profile = @user.profile
+  end
+
   private
     def profile_params
       params.require(:profile).permit(:first_name, :last_name, :avatar, :job_title, :phone_number, :contact_email, :description)
